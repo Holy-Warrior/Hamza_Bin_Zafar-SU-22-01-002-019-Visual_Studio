e8a60acf369bbd64ddff2ba92d89e147f01c9210
Holy-Warrior <hamzazafarnokia@gmail.com>
2025-07-07 12:48:59 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hamza";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is " + name + "!");
+        }
+    }
+}
